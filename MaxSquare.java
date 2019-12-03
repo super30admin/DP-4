b3d2b4a4dbf7f5f86a4852b1dff4da56ed3336a4
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-12-03 12:56:48 -0800

    Create MaxSquare.java

---
+++ MaxSquare.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows=matrix.length;
+        if(rows==0)return 0;
+        int cols=matrix[0].length;
+        int [][] dp = new int[rows+1][cols+1];
+        int max=0;
+        for(int i=1; i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j]=Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+                }
+                max=Math.max(max,dp[i][j]);
+            }
+        }
+        return max*max;
+    }
+}
+
+//Space complexity:O(m*n)
+//Time complexity:O(m*n)
