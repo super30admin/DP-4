ed363f96e8aa577c15cfb2be027f16532054f368
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2020-01-20 09:15:31 -0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,6 +1,7 @@
 // Time Complexity - O(mn) where m is the number of rows and n is the number of columns
 // Space Complexity- O(mn) where m is the number of rows and n is the number of columns; space used by the dp matrix
 // This Solution worked on LeetCode
+// To calculate the maximum length of the square, we need to find the minimum of the previous value vertically, horizontally and diagonally and add one to it. So, if the minimum is 0, it means a square of length 1 will be formed using the 1 element in process. and if the minimum is 1 it means a square of length 2 can be formed. and so forth. The maximum value obtained will the max length of the square.
 
 class Solution {
     public int maximalSquare(char[][] matrix) {
