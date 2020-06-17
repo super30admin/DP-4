7d362ccc0b2fd5e3d6acf8dc568b89a035439b44
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2020-06-16 18:42:21 -0700

    MaximalSquare - Dynamic Programming

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -1,23 +1,21 @@
-// Time Complexity : O(n^2); n - number of elements in the matrix
-// Space Complexity : O(1)
+// Time Complexity : O(n); n - number of elements in the matrix
+// Space Complexity : O(n); n - number of elements in the matrix
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Yes. Checking if the diagonal element is '1' throws 'Time Limit Exceeded' exception.
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
 /*
-	1. Find '1's in the matrix
-	2. Determine the diagonal element (Don't check if the diagonal element is '1')
-	3. Check both row and colums from the diagonal element, for '0's. If '0' not found, increase the counter,
-		will be reset to 1 at once we find '1' in Step1.
-	4. Repeat Step2 and Step3 untill a '0' is found. Keep track of Maximum (max) of counter at each Step1.
-	5. Return max*max
+	1. Initialize dp[m+1][n+1] and fill 1st row and 1st col with zeros (automatic in java as it's default value)
+	2. Find '1' in the original matrix
+	3. dp[i][j] = 1 + Min(top, top-left, left)
+	4. Keep track of maximum value throughout the dp matrix 
 */
 
 
 
 class MaximalSquare {
-	//Brute force Approach
+	//DP
 	public int maximalSquare(char[][] matrix) {
 		//edge
 		if(matrix == null || matrix.length == 0)
@@ -25,39 +23,14 @@ class MaximalSquare {
 		
 		int m = matrix.length;
 		int n = matrix[0].length;
-		boolean flag = false;
 		int maxSquare = 0;
-		int count = 1;
+		int[][] dp = new int[m+1][n+1]; 
 		
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				if(matrix[i][j] == '1') {
-					count = 1;
-					flag = true;
-					while(i+count < m && j+count < n && flag) {
-							
-						//row check
-						for(int k=j+count; k >= j; k--) {
-							if(matrix[i+count][k] != '1') {
-								flag = false;
-								break;
-							}
-						}
-
-						//col check
-						for(int k=i+count; k >= i; k--) {
-							if(matrix[k][j+count] != '1') {
-								flag = false;
-								break;
-							}
-						}
-
-						//increase count if all 1's
-						if(flag)
-							count++;
-						
-					}
-					maxSquare = Math.max(maxSquare, count);
+		for(int i = 1; i < m+1; i++) {
+			for(int j = 1; j < n+1; j++) {
+				if(matrix[i-1][j-1] == '1') {
+					dp[i][j] = 1 + Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+					maxSquare = Math.max(maxSquare, dp[i][j]);
 				}
 			}
 		}
