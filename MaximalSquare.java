c7d22038e1d36d1c643b798b174a4b1e2d92ef3f
akash-akki <akashpakki@gmail.com>
2020-08-02 01:30:39 -0500

    DP-4 done

---
+++ MaximalSquare.java
@@ -0,0 +1,34 @@
+
+//Time Complexity : O(m*n)
+//Space Complexity : O(m*n)
+//Did it run on leetcode : yes
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        
+        if(matrix == null || matrix.length == 0)
+            return 0;
+        
+          
+         int[][] dp = new int[matrix.length+1][matrix[0].length+1];
+          int max= Integer.MIN_VALUE;
+          for(int i = 1 ; i < dp.length ; i++){
+            for(int j = 1 ; j < dp[0].length ;j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1])) + 1 ;
+                }
+               
+                max = Math.max(max,dp[i][j]);
+                
+            }
+              
+              
+          }         
+                 
+           return max*max;
+                   
+         
+               
+     
+    }
+}
