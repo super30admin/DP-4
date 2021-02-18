0e53440ffb3e31f50bcb94d9f53f365768811e9d
Nilesh Deokar <76528900+nilesh-525@users.noreply.github.com>
2021-02-17 20:52:35 -0500

    Update Maximal_Square.java

--- Maximal_Square.java
+++ Maximal_Square.java
@@ -47,4 +47,30 @@ class Maximal_Square {
         
         return max*max;
     }
+    /*
+      Time : O(M * N)
+      Space : O(M*N) | dp array space
+      Leetcode : YES
+      
+    */
+    //DP Solution
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null) return -1;
+        int n = matrix[0].length;
+        int m = matrix.length;
+        int[][] dp = new int[m+1][n+1];
+        int max = 0;
+        
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    // choose best amount three
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        
+        return max* max;
+    }
 }
