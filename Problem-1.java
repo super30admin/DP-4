32c95ea46639dbdb781ee5daa909e64090f58267
Mannat Dev <dev.m@husky.neu.edu>
2021-04-15 16:51:56 -0400

    Completed

---
+++ Problem-1.java
@@ -0,0 +1,31 @@
+//Time Complexity : O(m*n)
+//Space Complexity : O(m*n)
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0)
+            return 0;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int[][] dp = new int[m+1][n+1];
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=1; i<dp.length; i++)
+        {
+            for(int j=1; j<dp[0].length; j++)
+            {
+                if(matrix[i-1][j-1] == '1')
+                {
+                dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                max = Math.max(max, dp[i][j]);
+                }
+                
+            }
+        }
+        
+       return max*max; 
+    }
+}
