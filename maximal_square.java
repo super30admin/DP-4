ab7cfca3e27b4fd3e22b54e05e9d52ba9102e668
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-06-15 14:52:41 -0400

    Update maximal_square.java

--- maximal_square.java
+++ maximal_square.java
@@ -38,3 +38,29 @@ class Solution {
         
     }
 }
+
+
+//Optimal solution
+//Time complexity: o(mxn)
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) return 0;
+        int n = matrix.length; int m = matrix[0].length;
+        int max = 0;
+        int [][] dp = new int[n+1][m+1];
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+                    max = Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        
+     
+        return max*max;
+        
+    }
+}
+
