548e8d18b2bbfe473ac60d61614a770045873c70
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-08-01 14:52:14 -0400

    added dp 4

---
+++ MaximalSquare.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        
+        int r = matrix.length
+        int c = matrix[0].length;
+        
+        int[][] dp = new int[r + 1][c + 1];
+        
+        int max = 0;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
