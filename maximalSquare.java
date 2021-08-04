f7196d478ca03d200c0de8cd3180126d8e67c482
Kishan Sinojia <kishansinojia@Kishans-MacBook.local>
2021-08-04 12:48:05 -0400

    Completed DP-4.

---
+++ maximalSquare.java
@@ -0,0 +1,22 @@
+// Time: O(mn)
+//Space: O(mn)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) return 0;
+        int m = matrix.length; int n = matrix[0].length;
+        
+        int[] dp = new int[n+1];
+        int max = 0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j])) + 1;
+                    max = Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        
+        return max*max;
+    }
+}
