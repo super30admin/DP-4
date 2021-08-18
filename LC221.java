f1079bb6b46657425f897c50d6d8172e7542630f
Manav Ahlawat <manavahlawat95@gmail.com>
2021-08-17 20:24:06 -0400

    DP-4. Please review.

---
+++ LC221.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = 0;
+        
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i = 1; i < m + 1; i++) {
+            for(int j = 1; j < n + 1; j++) {
+                if(matrix[i-1][j-1] == '1') {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max*max;
+    }
+}
+
+//Time : O(mn)
+//Space : O(mn)
