e3c57607f4eca60ddd8cc288503f2b79755eed21
Varun Gollapalli <gvarun1999@gmail.com>
2022-05-29 12:52:10 -0700

    DP 4 Submission
    Submission includes Maximal Square, Partition Array

---
+++ PartitionArray.java
@@ -0,0 +1,44 @@
+//TC = O(N)
+//SC = O(N)
+
+import java.util.*;
+public class PartitionArray {
+    
+    public static int maxSumAfterPartitioning(int[] arr, int k) {
+      
+        if(arr == null || arr.length == 0) return 0;
+        
+        int n = arr.length;
+        int[] dp = new int[n];
+        
+        dp[0]=arr[0];
+        
+        for(int i = 1;i<n;i++)
+        {
+            int max = arr[i];
+            for(int j = 1;j<=k && i-j+1>=0;j++)
+            {
+                max = Math.max(max, arr[i-j+1]);
+                
+                if(i-j>=0)
+                {
+                    dp[i] = Math.max(dp[i],max*j + dp[i-j]);
+                }
+                else
+                {
+                    dp[i] = Math.max(dp[i],max * j);
+                }
+            }
+        }
+        
+        return dp[n-1];
+        
+    }
+
+    public static void main(String args[])
+    {
+        int[] arr = {1,15,7,9,2,5,10};
+        System.out.println(maxSumAfterPartitioning(arr, 3));
+    }
+
+}
