a243706c6a3c34e147676c5f5e82200bfb125e2b
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-22 12:31:11 -0500

    Completed Sprint DP-4

---
+++ maximal_square.java
@@ -0,0 +1,31 @@
+// Time Complexity : O(mxn)
+// Space Complexity : O(mxn)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return 0;
+
+        int max = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        return max * max;
+
+    }
+} 
