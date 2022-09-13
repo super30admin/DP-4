46ce203b2f10f7d3c71c6e260d6cdd14cd1b941c
RAHUL UDHAYAKUMAR <65881610+RahulDeMaverick@users.noreply.github.com>
2022-09-12 22:30:20 -0400

    Create PartitionArrayMaxSum.java

---
+++ PartitionArrayMaxSum.java
@@ -0,0 +1,22 @@
+//tc : o(kn)
+//sc : o(n)
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        if(arr == null || arr.length ==0);
+        int n = arr.length;
+        int [] dp = new int[n];
+        dp[0] = arr[0];
+        for(int i=1;i<n;i++){
+            int max =arr[i];
+            for(int j=1;j<=k && i-j +1>=0;j++){
+                max = Math.max(max,arr[i-j+1]);
+                 int currPart = max*j;
+                if(i-j >=0)
+                     currPart = max*j + dp[i-j];
+                    
+                dp[i] = Math.max(dp[i],currPart);
+            }
+        }
+        return dp[n-1];
+    }
+}
