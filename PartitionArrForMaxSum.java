d959a557da609111f242a9aec2be83f4fe3bf755
Shubham Ayachit <shubham.s.ayachit@gmail.com>
2022-11-08 01:33:00 -0500

    DP-4Completed

---
+++ PartitionArrForMaxSum.java
@@ -0,0 +1,27 @@
+// Time Complexity : O(nk) n is length of string
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :
+
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        if(arr == null || arr.length == 0 || k == 0) return 0;
+
+        int n = arr.length;
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+
+        for(int i = 0; i<n; i++) {
+            int max = dp[i];
+            for(int j = 1; j<=k && i-j+1 >=0; j++) {
+                max = Math.max(max, arr[i-j+1]);
+                if(i-j >= 0){
+                    dp[i] = Math.max(dp[i], max * j + dp[i-j]);
+                } else {
+                    dp[i] = Math.max(dp[i], max * j);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
