5751fe23f22c7ded0c627ff90345fa9d2593d8b4
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-11-11 17:56:23 -0600

    Optimized approach added  - Leetcode 221

--- LeetCode_221_Maximal_Square.java
+++ LeetCode_221_Maximal_Square.java
@@ -124,3 +124,64 @@ class Solution {
     
     }
 }
+
+
+//approach -3
+//optimized DP on space
+/*
+approach-3 - space Optimized - DP solution
+initals - we take one dp array of type int[], and size will be n+1 to avoid the boundary condition check. 
+initialize max =0. 
+
+before every j loop starts , we have diagonal reset to zero. 
+1. arr[0] is the diagonal value and that's the extra col we added to avoid the boundary check. 
+2. we are at i=0, j=0 in matrix , and we want to fill arr[j+1] = arr[1]
+3. matrix[i][j] is 1;  then we calculate arr[j+1] 's value in temp  =min of [arr[j], arr[j+1], diagonal ] +1;
+4. we change diagonal = arr[j+1], and arr[j+1] =temp
+--- at setp -3 if we are standing at 0; we just update diagonal = arr[j+1], update arr[j+1]  =0 and move on. 
+
+5. max = math.max(arr[j+1], max);
+after we exhaust loop, 
+6. at last we just return the max*max
+
+
+Note - remeber we can not do this modification in place, as given is charcter matrix and once we do addition, that becomes a string!!
+
+TC - O(m*n) traversal
+sc - O(n) - dp array readuced from earlier approach of (O(mn))
+*/
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) return 0;
+        
+        int max =0;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] arr = new int[n+1]; // by default will be zero!
+        
+        
+        for(int i=0; i< m; i++)
+        {
+            int dig = 0;
+            for (int j=0; j< n; j++)
+            {
+               if(matrix[i][j] == '1')
+               {
+                   int temp = Math.min(dig , Math.min(arr[j], arr[j+1])) + 1;
+                   dig = arr[j+1];
+                   arr[j+1] = temp;
+                   max = Math.max(arr[j+1], max);
+               }
+                else
+               {
+                   dig = arr[j+1];
+                   arr[j+1] = 0;
+               }
+            }
+        }
+        return max*max;
+    
+    }
+}
