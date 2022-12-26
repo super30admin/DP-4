97ec76433917b0495644056f73c9c7c2869272b9
Khushboo Gandhi <khushboo1028@gmail.com>
2022-12-25 22:36:08 -0800

    DP 4 done

---
+++ partition-array.java
@@ -0,0 +1,29 @@
+//tc is O(n)
+//sc is (n)
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        if (arr == null || arr.length == 0)
+            return 0;
+
+        int n = arr.length;
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            int max = arr[i];
+            for (int j = 1; j <= k && i - j + 1 >= 0; j++) {
+                max = Math.max(arr[i - j + 1], max);
+                if (i - j >= 0) {
+                    dp[i] = Math.max(dp[i], max * j + dp[i - j]);
+                } else {
+                    dp[i] = Math.max(dp[i], max * j);
+                }
+
+            }
+
+        }
+
+        return dp[n - 1];
+
+    }
+}
